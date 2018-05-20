aedc58fa0dfc2e890a78f6ee5911804cf944fe9e
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-05-20 11:00:46 +1000

    Configure graphql and salesforce org

--- index.js
+++ index.js
@@ -4,12 +4,49 @@
 const express = require('express')
 const assert = require('assert')
 
+// Express-graphql
+const graphqlHTTP = require('express-graphql')
+// Import root schema
+const rootSchema = require('./schema/root')
+
 // Initialise express
 const server = express()
 const PORT = process.env.PORT || 4000
 
-// GraphQL middleware
-server.use('/graphql', require('./routes/graphql'))
+
+// Initialise dotenv
+require('dotenv').config()
+
+// Initialise nForce and connection parameters for Salesforce Connected App
+const nforce = require('nforce')
+const org = nforce.createConnection({
+  clientId: process.env.CONSUMER_KEY,
+  clientSecret: process.env.CONSUMER_SECRET,
+  redirectUri: 'https://localhost:3000/oauth/_callback',
+  environment: 'production',
+  mode: 'single'
+})
+const salesforce = require('./database/salesforce')(org)
+
+// Authenticate to salesforce using single user mode
+org.authenticate(
+  {
+    username: process.env.SALESFORCE_USERNAME,
+    password: process.env.SALESFORCE_PASSWORD,
+    securityToken: process.env.SALESFORCE_SECURITY_TOKEN
+  },
+  (error, response) => {
+    assert.equal(error, null)
+    // GraphQL middleware
+    server.use('/graphql', (req, res) => {
+      graphqlHTTP({
+        schema: rootSchema,
+        graphiql: true,
+        context: { salesforce }
+      })(req, res)
+    })
+  }
+)
 
 // Error handler
 server.use((error, req, res, next) => {
