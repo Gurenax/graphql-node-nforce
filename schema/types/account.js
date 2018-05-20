2fae667b1ee06e01573baf2994c812ecd3043f18
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-05-20 10:59:54 +1000

    Add Account type

---
+++ schema/types/account.js
@@ -0,0 +1,18 @@
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString
+} = require('graphql')
+
+const salesforce = require('../../database/salesforce')
+
+module.exports = new GraphQLObjectType({
+  name: 'Account',
+  fields: () => {
+    return {
+      id: { type: GraphQLID },
+      name: { type: GraphQLString },
+      sla__c: { type: GraphQLString }
+    }
+  }
+})
