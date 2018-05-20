f664a23fce5d909f13c93571b6b422ac575de7e4
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-05-20 11:06:33 +1000

    Refactor mapRecordsToFields

--- database/salesforce.js
+++ database/salesforce.js
@@ -17,11 +17,9 @@ module.exports = org => {
  * @param {list} records - list of records from salesforce response
  * @return {list} list of records which correspond to their fields
  */
-const mapRecordsToFields = records => {
-  return records.reduce((result, record) => {
-    return [...result, record._fields]
-  }, [])
-}
+const mapRecordsToFields = records => (
+  records.reduce((result, record) => [...result, record._fields]), []
+)
 
 /**
  * Perform a SOQL Query
