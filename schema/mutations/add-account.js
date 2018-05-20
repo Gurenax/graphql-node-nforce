2b58a84042778c675e8ad60158ef256b2a5deb3d
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-05-20 15:49:06 +1000

    Add account creation mutation type

---
+++ schema/mutations/add-account.js
@@ -0,0 +1,26 @@
+const {
+  GraphQLInputObjectType,
+  GraphQLString,
+  GraphQLNonNull
+} = require('graphql')
+
+
+const AccountType = require('../types/account')
+
+const AccountInputType = new GraphQLInputObjectType({
+  name: 'AccountInput',
+  fields: {
+    name: { type: GraphQLString },
+    sla__c: { type: GraphQLString }
+  }
+})
+
+module.exports = {
+  type: AccountType,
+  args: {
+    input: { type: new GraphQLNonNull(AccountInputType) }
+  },
+  resolve: (obj, { input }, { salesforce }) => {
+    return salesforce.createAccount(input)
+  }
+}
