b600ce430937d9b18269286619c2170559ea4168
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-05-21 20:35:18 +1000

    Refactor Acount Input Type into a separate folder

---
+++ schema/inputs/account-input.js
@@ -0,0 +1,14 @@
+const {
+  GraphQLInputObjectType,
+  GraphQLID,
+  GraphQLString
+} = require('graphql')
+
+module.exports = new GraphQLInputObjectType({
+  name: 'AccountInput',
+  fields: {
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    sla__c: { type: GraphQLString }
+  }
+})
